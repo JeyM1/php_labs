4dc4635a46536b7476baf6a5b81731b78f50c306
JeyM1 <denafen2000@gmail.com>
2020-03-30 18:06:24 +0300

    bugfix

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
 	$('#nav-icon1').click(function(){
 		$(this).toggleClass('open');
-		$("#mob-menu").toggleClass("open");
-		$(".back-layer").fadeToggle('fast')
+		$('#mob-menu').toggleClass('open');
+		$('.back-layer').fadeToggle('fast');
 	});
-
 });
+
+$(window).resize(function(){
+	const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if(vw && vw >= 992) {
+		$('#nav-icon1').removeClass('open');
+		$('#mob-menu').removeClass('open');
+		$('.back-layer').fadeOut();
+	}
+})
