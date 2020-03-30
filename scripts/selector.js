173ab8b85c0ac953f903f0d7f6de4cac7299462b
JeyM1 <denafen2000@gmail.com>
2020-03-30 16:40:07 +0300

    added mobile selector

--- scripts/selector.js
+++ scripts/selector.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 	$('#nav-icon1').click(function(){
 		$(this).toggleClass('open');
+		$("#mob-menu").toggleClass("open");
+		$(".back-layer").fadeToggle('fast')
 	});
+
 });
